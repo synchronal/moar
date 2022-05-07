617310480cd51a6e4c6be10286bbfc008f4d1ed1
Erik Hanson <erik@eahanson.com>
2022-05-06 17:25:12 -0700

    GitHub actions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,6 @@
 
 ## Unreleased changes
 
-## 1.0.0
+## 0.1.0
 
 - Initial release
--- mix.exs
+++ mix.exs
@@ -1,15 +1,25 @@
 defmodule Moar.MixProject do
   use Mix.Project
 
+  @scm_url "https://github.com/synchronal/pages"
+  @version "0.1.0"
+
   def project do
     [
       app: :moar,
       deps: deps(),
+      description: "A collection of useful functions.",
       dialyzer: dialyzer(),
+      docs: docs(),
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
+      homepage_url: @scm_url,
+      name: "Moar",
+      package: package(),
+      preferred_cli_env: [credo: :test, dialyzer: :test],
+      source_url: @scm_url,
       start_permanent: Mix.env() == :prod,
-      version: "1.0.0"
+      version: @version
     ]
   end
 
@@ -21,7 +31,7 @@ defmodule Moar.MixProject do
 
   defp deps do
     [
-      {:credo, "~> 1.6", only: :dev, runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.28.4", only: :dev, runtime: false},
       {:mix_audit, "~> 1.0", only: :dev, runtime: false}
@@ -30,9 +40,24 @@ defmodule Moar.MixProject do
 
   defp dialyzer, do: [plt_add_apps: [:ex_unit]]
 
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md", "LICENSE.md"]
+    ]
+  end
+
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   defp extra_applications(:test), do: [:crypto, :logger]
   defp extra_applications(_), do: [:logger]
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: ["synchronal.dev", "Erik Hanson", "Eric Saxby"],
+      links: %{"GitHub" => @scm_url}
+    ]
+  end
 end
