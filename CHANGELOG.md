a27d81868601f6e60504a230b625a00f0ae90bae
Erik Hanson <erik@eahanson.com>
2022-06-21 18:24:01 -0700

    Release v1.17.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## Unreleased changes
 
+## 1.17.0
+
 - Add `ignore_whitespace: :leading_and_trailing` option to `Moar.Assertions.assert_eq`
 
 ## 1.16.0
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Moar.MixProject do
   use Mix.Project
 
   @scm_url "https://github.com/synchronal/moar"
-  @version "1.16.0"
+  @version "1.17.0"
 
   def project do
     [
