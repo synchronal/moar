4f5923771b47b8a5773b1d230f9733f5e9ee77f0
Eric Saxby <sax@livinginthepast.org>
2023-12-22 14:45:49 -0800

    Disable PredicateFunctionNames credo check

--- .credo.exs
+++ .credo.exs
@@ -103,7 +103,7 @@
           {Credo.Check.Readability.ParenthesesInCondition, []},
           {Credo.Check.Readability.ParenthesesOnZeroArityDefs, []},
           {Credo.Check.Readability.PipeIntoAnonymousFunctions, []},
-          {Credo.Check.Readability.PredicateFunctionNames, []},
+          {Credo.Check.Readability.PredicateFunctionNames, false},
           {Credo.Check.Readability.PreferImplicitTry, []},
           {Credo.Check.Readability.RedundantBlankLines, []},
           {Credo.Check.Readability.Semicolons, []},
